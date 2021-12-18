641c755a0d60826da305aa35ab22afef277d7602
Per-Bjørnar Iversen <per.b.iversen@gmail.com>
2021-12-18 23:03:21 +0100

    Added @jsonignore to username

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -19,6 +19,7 @@ public class Author {
     private Long id;
     private String firstname;
     private String lastname;
+    @JsonIgnore
     private String username;
     @JsonIgnore
     private String password;
